876d86c0fba741ed342ace1baf59dfd594cc9706
mohdayaankhan <ayankhan6265300@gmail.com>
2026-04-29 14:46:40 +0530

    Add files via upload

---
+++ declare.js
@@ -0,0 +1,4 @@
+document.write("Welcome to external JS")
+document.write("<h1 style='color: rgb(6, 6, 255);background-color: black;'>Welcome to js insaid body tag </h1>")
+document.write("<br>")
+document.write("Welcome to js insaid body tag")
